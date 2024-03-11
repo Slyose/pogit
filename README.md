3478ffc2ff8f1f3b34b4f4fefbc86497d1805302
Slyose <modour498@gmail.com>
2024-03-11 02:38:52 +0000

    feat: Implement core functionality for cloning a repo and setting local git configs, as well as a command for resetting local data

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # pogit
-Created to make using git with two accounts on the same mac much easier
+
+pogit was created to make using git with two accounts on the same mac much easier.
+
+pogit simplifies managing the use of git with multiple GitHub accounts on the same machine, specifically tailored for macOS users.
+
+It enables seamless switching between GitHub accounts for cloning and managing repositories without the hassle of manually adjusting SSH configurations, and local git configs.
+
+## Usage
+
+- Install globally by cloning, and running `npm install -g` when with the cwd set as the repo, then use below commands
+
+- `pogit clone <github repo url> <?file path>` **URL must be HTTPS,** _file path is optional._
+  - This command will ask you to choose a GitHub account, by accessing your configured SSH profiles.
+  - example use `pogit clone https://github.com/Slyose/pogit`
+
+- `pogit reset`
+  - This will clear all local username and email data - use if you've made an error inputting any details.
+
+## Uninstallation
+
+- Run `npm uninstall pogit -g` to uninstall the package.
+
+## Setup
+
+This repo will eventually be able to guide one through the setup process for multiple GitHub accounts on one PC, but for now, you must manually configure SSH profiles via the steps described in [this guide](https://www.linkedin.com/pulse/multiple-github-accounts-same-mac-hesham-osama) (or similar).
+
+## To-do
+
+- [ ] Implement SSH URL Support for `clone` command
+
+  - e.g., _pogit clone git@github.com:Slyose/pogit.git_
+    - would have to change *git@github.com* to use the appropriate SSH profile instead of _generic github.com_
+
+- [ ] Automate SSH Profile Configuration: implement `pogit configure` command to allow a user to configure multiple SSH profiles from scratch.
+
+- [ ] `pogit set remote` to change the URL of a repo AND the local config details to whichever GitHub profile they want.
+
+- [ ] Implement support for non-standardized HostNames e.g., _'Host googlymoogly'_.
+
+- [ ] General clean up, more consistent use of async/await over promises, more modularisation, & graceful error handling
---
+++ index.js
@@ -0,0 +1,151 @@
+#!/usr/bin/env node
+
+const inquirer = require("inquirer");
+const { exec } = require("child_process");
+const { readFile } = require("fs/promises");
+const { writeFileSync } = require("fs");
+const os = require("os");
+const pogitConfigData = require(`${__dirname}/.pogitconfig.json`);
+
+let localRepoName = null;
+if (process.argv[4]) {
+  localRepoName = process.argv[4];
+}
+
+const hostRegex = /(?<=Host\s)[\w.-]+/g;
+
+function main() {
+  const keyword = process.argv[2];
+
+  switch (keyword) {
+    case "clone":
+      clone();
+      break;
+    case "reset":
+      resetConfigFile();
+      break;
+
+    default:
+      console.log("command not found");
+      break;
+  }
+}
+
+async function emailSetup(chosenHost) {
+  console.log("No email found for chosen alias. Initiating one time setup.");
+  const { email } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "email",
+      message: `Please input an email for ${chosenHost}:`,
+    },
+  ]);
+  const { username } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "username",
+      message: `Please input an github username for ${chosenHost} (exactly as it appears on github):`,
+    },
+  ]);
+  pogitConfigData[chosenHost] = {
+    email,
+    username,
+  };
+  writeFileSync(
+    `${__dirname}/.pogitconfig.json`,
+    JSON.stringify(pogitConfigData),
+    (err) => {
+      if (err) {
+        console.log("Error writing file", err);
+      } else {
+        console.log("Successfully added new user email data.");
+      }
+    }
+  );
+}
+
+function clone() {
+  let chosenUsername = "";
+  let chosenHost = "";
+
+  readFile(`${os.homedir()}/.ssh/config`)
+    .then((configInfo) => {
+      const sshConfig = configInfo.toString();
+      const hosts = sshConfig.match(hostRegex) || [];
+      if (hosts.length === 0) {
+        console.log("No hosts found in the SSH configuration.");
+        return;
+      }
+      const repoUrl = process.argv[3].match(/github\.com\/(.*)/s)[1];
+      if (!localRepoName) {
+        localRepoName = repoUrl.match(/[^\/]+$/)[0];
+      }
+      const usernames = hosts.map((host) => {
+        return host.match(/github\.com-(.+)/)[1];
+      });
+
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "selectedHost",
+            message: "Please choose a github alias:",
+            choices: hosts,
+          },
+        ])
+        .then(async (answers) => {
+          console.log("You selected:", answers.selectedHost);
+          chosenHost = answers.selectedHost;
+          if (!(chosenHost in pogitConfigData)) {
+            await emailSetup(chosenHost);
+          }
+          chosenUsername = usernames[answers.selectedHost];
+          const cloneCommand = `git clone git@${chosenHost}:${repoUrl}.git ${localRepoName}`;
+          return cloneCommand;
+        })
+        .then((cloneCommand) => {
+          return exec(cloneCommand, (error, stdout, stderr) => {
+            if (error) {
+              console.error(`exec error: ${error}`);
+              return;
+            }
+            // console.log(`stdout: ${stdout}`);
+            console.error(`${stderr}`);
+            exec(
+              `cd ${localRepoName} && git config user.name '${pogitConfigData[chosenHost].username}' && git config user.email '${pogitConfigData[chosenHost].email}'`,
+              (error, stdout, stderr) => {
+                if (error) {
+                  console.error(`exec error: ${error}`);
+                  return;
+                }
+                // console.log(`stdout: ${stdout}`);
+                // console.error(`stderr: ${stderr}`);
+              }
+            );
+          });
+        })
+        .catch((error) => {
+          if (error.isTtyError) {
+            console.log(
+              "Prompt couldn't be rendered in the current environment"
+            );
+          } else {
+            console.error("An error occurred:", error);
+          }
+        });
+    })
+    .catch((error) => {
+      console.error("Failed to read SSH config file:", error);
+    });
+}
+
+async function resetConfigFile() {
+  try {
+    await writeFile(configFilePath, JSON.stringify({}), "utf8");
+    console.log("Config file has been reset to empty.");
+  } catch (error) {
+    console.error("An error occurred while resetting the config file:", error);
+  }
+}
+
+main();
